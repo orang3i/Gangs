4f6c53ae000c0cc36e95e4bc921612a84f4dd549
orang3i <orangeinfotainment@gmail.com>
2023-09-06 17:43:14 +0530

    testing shadow output

--- src/main/java/com/orang3i/gangs/Gangs.java
+++ src/main/java/com/orang3i/gangs/Gangs.java
@@ -3,12 +3,16 @@ package com.orang3i.gangs;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.TextColor;
+import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static net.kyori.adventure.text.Component.text;
 
@@ -24,6 +28,27 @@ public final class Gangs extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        File datafolder = new File(this.getDataFolder() + "/");
+        File config = new File(getDataFolder(), "config.yml");
+        if(!datafolder.exists()){
+            datafolder.mkdir();
+            try {
+                InputStream stream = this.getResource("config.yml");
+                FileUtils.copyInputStreamToFile(stream, new File(this.getDataFolder(),"config.yml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (!config.isFile()) {
+            try {
+                InputStream stream = this.getResource("config.yml");
+                FileUtils.copyInputStreamToFile(stream, new File(this.getDataFolder(),"config.yml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String dbType = this.getConfig().getString("database.type");
+        System.out.println(dbType);
         this.adventure = BukkitAudiences.create(this);
         getCommand("tester").setExecutor(new Tester(this));
         System.out.println("yessir");
