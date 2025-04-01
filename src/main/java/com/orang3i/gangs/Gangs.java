7e5b2708be3e30e1f28d556baa6fbaa7695fabad
orang3i <orangeinfotainment@gmail.com>
2025-04-01 20:41:36 +0530

    first commit

---
+++ src/main/java/com/orang3i/gangs/Gangs.java
@@ -0,0 +1,17 @@
+package com.orang3i.gangs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gangs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
