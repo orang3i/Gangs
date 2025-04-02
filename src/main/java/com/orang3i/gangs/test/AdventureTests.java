84edb4054ed3c6772b03f05fb28fdfe647c69882
orang3i <orangeinfotainment@gmail.com>
2025-04-02 11:28:28 +0530

    fix import

--- src/main/java/com/orang3i/gangs/test/AdventureTests.java
+++ src/main/java/com/orang3i/gangs/test/AdventureTests.java
@@ -1,17 +1,14 @@
 package com.orang3i.gangs.test;
 
 import com.orang3i.gangs.Gangs;
+import com.orang3i.gangs.adventure.MiniMessageDeserializer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
 import java.sql.SQLException;
-
-import static com.orang3i.gangs.adventure.MiniMessageDeserializer.mm;
-
 
 public class AdventureTests implements Listener {
 
@@ -24,7 +21,7 @@ public class AdventureTests implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        Component welcome = mm("Hello, you joined orang3i's test server!",true).clickEvent(ClickEvent.callback(audience -> {
+        Component welcome = MiniMessageDeserializer.mm("Hello, you joined orang3i's test server!",true).clickEvent(ClickEvent.callback(audience -> {
             try {
                 secretMessage("You found the secret",player);
             } catch (SQLException e) {
@@ -35,7 +32,7 @@ public class AdventureTests implements Listener {
     }
 
     private void secretMessage(String message , Player player) throws SQLException {
-        player.sendMessage(mm(message));
+        player.sendMessage(MiniMessageDeserializer.mm(message));
         plugin.getDAO().createTestTable();
         plugin.getDAO().insertData();
     }
